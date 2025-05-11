48994ac9d81b4b9808213651d2d6c76d32695075
NathanVendrame <ncvendrame@gmail.com>
2025-05-10 22:52:24 -0300

    Project creation

---
+++ src/main/java/br/com/fiap/techrestaurant/TechrestaurantApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.techrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechrestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechrestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/techrestaurant/TechrestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.techrestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechrestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
